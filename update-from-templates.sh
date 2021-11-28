100b68ebae92c9615769bbd976e75724c001ea0f
Gunnar Morling <gunnar.morling@googlemail.com>
2021-11-28 17:41:21 +0100

    🤷 Adding .gitignore

--- update-from-templates.sh
+++ update-from-templates.sh
@@ -21,7 +21,8 @@ set -eo pipefail
 
 mvn clean verify -f oss-quickstart-template-simple/pom.xml
 mvn archetype:create-from-project -f oss-quickstart-template-simple/pom.xml
-cp -r oss-quickstart-template-simple/target/generated-sources/archetype/src oss-quickstart-archetype-simple
+cp -r oss-quickstart-template-simple/target/generated-sources/archetype/src/main/resources/archetype-resources oss-quickstart-archetype-simple/src/main/resources
+# cp -r oss-quickstart-template-simple/.gitignore oss-quickstart-archetype-simple/src/main/resources/archetype-resources
 
 # The archetype creation process drops the line-wrap after the license header in the pom.xml; adding this back
 # using gsed on macOS to have flag compatibility with gnu sed on Linux
