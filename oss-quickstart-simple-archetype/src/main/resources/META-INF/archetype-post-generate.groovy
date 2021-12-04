0635cf5794e41aca598bcb382138ad5759ddac62
Gunnar Morling <gunnar.morling@googlemail.com>
2021-12-04 19:04:28 +0100

    👷‍♂️ No need to adjust module-info.java after the fact

--- oss-quickstart-simple-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ oss-quickstart-simple-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,15 +1,3 @@
 def file = new File(request.outputDirectory, request.artifactId + '/.gitignore.tmpl')
 def gitIgnoreFile = new File(request.outputDirectory, request.artifactId + '/.gitignore')
 file.renameTo(gitIgnoreFile)
-
-def moduleName = request.properties['moduleName']
-def packagePath = request.package.replaceAll('\\.', '/')
-def moduleInfoFile = new File(request.outputDirectory, request.artifactId + '/src/main/java/' + packagePath + '/module-info.java')
-
-// module-info.java gets moved into the package of the application; move it back to src/main/java
-if (!moduleName || moduleName == 'NONE') {
-    moduleInfoFile.delete()
-} else {
-    def moduleInfoFileNew = new File(request.outputDirectory, request.artifactId + '/src/main/java/module-info.java')
-    moduleInfoFile.renameTo(moduleInfoFileNew)
-}
