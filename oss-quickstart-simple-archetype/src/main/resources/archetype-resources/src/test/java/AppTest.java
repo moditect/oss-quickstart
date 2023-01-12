15208017d26c10ac47df1a5ec60ac466f1a6aa76
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-01-12 17:37:35 +0100

    Use package-private for Jupiter tests
    Jupiter fosters the use of package-private modifiers for test classes and methods (SonarQube also fosters that).

--- oss-quickstart-simple-archetype/src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ oss-quickstart-simple-archetype/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -22,10 +22,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class AppTest {
+class AppTest {
 
     @Test
-    public void helloShouldReturnName() {
+    void helloShouldReturnName() {
         App app = new App();
         assertThat(app.hello("Bob")).isEqualTo("Hello, Bob");
     }
